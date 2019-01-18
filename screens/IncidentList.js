609767b889b907deadf537a1d39e223465bee82c
hyperthreading <no1expman@gmail.com>
2019-01-18 14:25:06 +0900

    Fill the bottom space

--- screens/IncidentList.js
+++ screens/IncidentList.js
@@ -10,10 +10,10 @@ import {
 } from 'react-native';
 import { Icon } from '@shoutem/ui';
 
-import { getBottomSpace } from 'react-native-iphone-x-helper'
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import Layout from '../constants/Layout';
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
 
 import Incident from '../components/Incident';
 
@@ -34,13 +34,20 @@ export default class IncidentList extends React.Component {
             <Icon name="settings" />
           </View>
           <ScrollView style={{ flex: 1 }}>
-            {[...Array(8)].map(() => <Incident onPress={() => this.props.navigation.navigate('IncidentDetail')}/>)}
+            {[...Array(8)].map(() => (
+              <Incident
+                onPress={() => this.props.navigation.navigate('IncidentDetail')}
+              />
+            ))}
           </ScrollView>
         </SafeAreaView>
-        <TouchableOpacity style={styles.reportButton} onPress={() => this.props.navigation.navigate('Modal')}>
+        <TouchableOpacity
+          style={styles.reportButton}
+          onPress={() => this.props.navigation.navigate('Modal')}
+        >
           <Text style={styles.reportButtonText}>Send Report</Text>
         </TouchableOpacity>
-        <View style={{ backgroundColor: 'black', height: getBottomSpace() }}></View>
+        <View style={styles.iPhoneBottomFill} />
       </View>
     );
   }
@@ -50,7 +57,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch'
+    alignItems: 'stretch',
   },
   notchMargin: { height: Platform.select({ android: 40, ios: 0 }) },
   headerContainer: { margin: 8, marginLeft: 16, flexDirection: 'row' },
@@ -61,7 +68,8 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     alignItems: 'center',
     padding: 16,
-    paddingBottom: 16
+    paddingBottom: 16,
   },
   reportButtonText: { color: 'white', fontWeight: '500', fontSize: 24 },
+  iPhoneBottomFill: { backgroundColor: 'black', height: getBottomSpace() },
 });
