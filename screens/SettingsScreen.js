2d3179925826b46293a8e56a56598271143b3496
hyperthreading <no1expman@gmail.com>
2019-01-22 15:19:58 +0900

    Fix Safe Margin for Settings Screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView } from 'react-navigation'
 import SwitchToggle from 'react-native-switch-toggle';
 import { Ionicons } from '@expo/vector-icons';
 
