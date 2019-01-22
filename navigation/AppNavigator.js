e59b5e871f482d0585d305f2ab2c97518d55d222
Minseong Choi <chanm1996@gmail.com>
2019-01-22 21:03:57 +0900

    Re-activate Logins Screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -19,7 +19,7 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    // Auth: Login,
+    Auth: Login,
     App: createStackNavigator(
       {
         Main: createStackNavigator(
