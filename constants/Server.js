526ebc0565efb1e72975273db3489b2fa7c2e7b6
hyperthreading <no1expman@gmail.com>
2019-01-27 19:51:44 +0900

    Update server URL

--- constants/Server.js
+++ constants/Server.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-// export const URL = 'http://0aafb50b.ngrok.io';
 export const URL = 'https://lambda2.kaist.ac.kr';
-// export const URL = 'https://f7d20371.ngrok.io';
-=======
-export const URL = 'https://lambda2.kaist.ac.kr';
->>>>>>> 07307c808cd2aa674e450902526a4c28ab474adf
+
