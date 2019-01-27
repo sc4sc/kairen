388de301fd00e3c6ff017fc2f1ece088983aed4c
hyperthreading <no1expman@gmail.com>
2019-01-27 19:50:42 +0900

    Update server to deployed one

--- constants/Server.js
+++ constants/Server.js
@@ -1,3 +1,3 @@
 // export const URL = 'http://0aafb50b.ngrok.io';
-export const URL = 'https://61f57a86.ngrok.io';
+export const URL = 'https://lambda2.kaist.ac.kr';
 // export const URL = 'https://f7d20371.ngrok.io';
