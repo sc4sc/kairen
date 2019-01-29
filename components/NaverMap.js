1b63ca4ea945defdf074905124dcfdf02630006f
hyperthreading <no1expman@gmail.com>
2019-01-29 16:58:25 +0900

    Add NaverMap component

---
+++ components/NaverMap.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Text, TouchableOpacity, View, WebView } from 'react-native';
+
+const htmlAsset = Expo.Asset.fromModule(require('../assets/map.html'));
+
+async function loadHtml() {
+  let uri = '';
+  if (__DEV__) {
+    uri = htmlAsset.uri;
+  } else {
+    if (!htmlAsset.localUri) {
+      await htmlAsset.downloadAsync();
+      console.log('Downloading map.html...');
+    }
+    uri = htmlAsset.localUri;
+  }
+  console.log('Map URI: ' + uri);
+
+  const response = await fetch(uri);
+  console.log('Downloaded map.html');
+  return response.text();
+}
+
+export default class NaverMap extends React.Component {
+  state = { html: '<p>Loading</p>', hide: false };
+
+  componentWillMount = async () => {
+    loadHtml().then(html => {
+      this.setState({ html });
+    });
+  };
+
+  onPress = () => {
+    this.postAction({
+      type: 'panTo',
+      payload: { coords: { lat: 36.37334626411133, lng: 127.36397930294454 } },
+    });
+  };
+
+  onWebViewInit = () => {
+    this.postAction({
+      type: 'panTo',
+      payload: { coords: { lat: 36.37334626411133, lng: 127.36397930294454 } },
+    });
+  };
+
+  postAction = action => this._webview.postMessage(JSON.stringify(action));
+
+  // https://medium.com/@azharuddin31/react-native-pass-data-to-webview-and-get-data-out-of-webview-792ffbe7eb75
+  handleMessage = ({ nativeEvent }) => {
+    console.log('handleMessage', nativeEvent.data);
+    switch (nativeEvent.data) {
+      case 'ping': {
+        this.postAction({ type: 'pong' });
+        this.onWebViewInit();
+        return;
+      }
+    }
+  };
+
+  render() {
+    return (
+      <View style={this.props.style}>
+        <TouchableOpacity style={{ marginTop: 30 }} onPress={this.onPress}>
+          <Text>Go To KAIST</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ marginTop: 10 }}
+          onPress={() =>
+            this.setState(s => ({
+              hide: !s.hide,
+            }))
+          }
+        >
+          <Text>{this.state.hide ? 'Show' : 'Hide'}</Text>
+        </TouchableOpacity>
+        {this.state.hide ? null : (
+          <WebView
+            ref={webview => {
+              this._webview = webview;
+            }}
+            source={{
+              html: this.state.html,
+              baseUrl: 'http://localhost',
+            }}
+            style={{ flex: 1 }}
+            onMessage={this.handleMessage}
+          />
+        )}
+      </View>
+    );
+  }
+}
--- screens/MapTest.js
+++ screens/MapTest.js
@@ -2,63 +2,12 @@ import React from 'react';
 import { View, WebView, TouchableOpacity, Text } from 'react-native';
 
 import Layout from '../constants/Layout';
-
-const htmlAsset = Expo.Asset.fromModule(require('../assets/map.html'));
+import NaverMap from "../components/NaverMap";
 
 export default class MapTest extends React.Component {
-  state = { html: '<p>Loading</p>' };
-
-  componentWillMount = async () => {
-    let uri = '';
-    if (__DEV__) {
-      uri = htmlAsset.uri;
-    } else {
-      if (!htmlAsset.localUri) {
-        await htmlAsset.downloadAsync();
-        console.log('Downloading map.html...');
-      }
-      uri = htmlAsset.localUri;
-    }
-    console.log('Map URI: ' + uri);
-
-    const response = await fetch(uri);
-    console.log('Downloaded map.html');
-    this.setState({ html: await response.text() });
-  };
-
-  onPress = () => {
-    this.postAction({
-      type: 'panTo',
-      payload: { coords: { lat: 36.37334626411133, lng: 127.36397930294454 }},
-    });
-  };
-
-  postAction = action => this._webview.postMessage(JSON.stringify(action));
-
-  // https://medium.com/@azharuddin31/react-native-pass-data-to-webview-and-get-data-out-of-webview-792ffbe7eb75
-  handleMessage = ({ nativeEvent }) => {
-    console.log('handleMessage', nativeEvent.data);
-  };
+  state = { html: '<p>Loading</p>', hide: false };
 
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'stretch' }}>
-        <TouchableOpacity style={{ marginTop: 100 }} onPress={this.onPress}>
-          <Text>Touch me</Text>
-        </TouchableOpacity>
-        <WebView
-          ref={webview => {
-            this._webview = webview;
-          }}
-          source={{
-            html: this.state.html,
-            baseUrl: 'http://localhost',
-          }}
-          style={{ flex: 1 }}
-          onError={err => console.log('onError', err)}
-          onMessage={this.handleMessage}
-        />
-      </View>
-    );
+    return <NaverMap style={{ flex: 1, alignItems: 'center' }} />;
   }
 }
