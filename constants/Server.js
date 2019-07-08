12185887becb49bdaf2da6020f06f67aee6fda94
Jinwoo Jeong (Leo) <thinkratomos@gmail.com>
2019-07-08 22:39:28 +0900

    Change server to LB.

--- constants/Server.js
+++ constants/Server.js
@@ -9,8 +9,8 @@ if (
   URL = 'https://lambda2.kaist.ac.kr';
 } else {
   // Development / Test Apps
-  // URL = 'https://kairen.kaist.ac.kr/api';
-  URL = 'http://kairen.kaist.ac.kr:12422';
+  URL = 'https://kairen.kaist.ac.kr/api';
+  // URL = 'http://kairen.kaist.ac.kr:12422';
 }
 
 export default URL;
