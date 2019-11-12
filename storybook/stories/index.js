608976785eec8f3a90ba6575e1ae09a5c91add27
hyperthreading <no1expman@gmail.com>
2019-11-12 21:44:06 +0900

    add incident card as an example

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
+import { Text } from 'react-native'
 
-import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react-native'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
 // eslint-disable-next-line import/extensions
-import Button from './Button';
-import CenterView from './CenterView';
-import Welcome from './Welcome';
+import Button from './Button'
+import CenterView from './CenterView'
+import Welcome from './Welcome'
+import IncidentCard from '../../components/IncidentCard'
+import { sampleIncident } from '../../constants/SampleIncidents'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
 
 storiesOf('Button', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
@@ -23,4 +27,19 @@ storiesOf('Button', module)
     <Button onPress={action('clicked-emoji')}>
       <Text>😀 😎 👍 💯</Text>
     </Button>
-  ));
+  ))
+
+storiesOf('IncidentCard', module)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add('on checking incident', () => {
+    const data = { ...sampleIncident[0], state: '확인중' }
+    return <IncidentCard data={data} />;
+  })
+  .add('on handling incident', () => {
+    const data = { ...sampleIncident[0], state: '처리중' }
+    return <IncidentCard data={data} />;
+  })
+  .add('on complete incident', () => {
+    const data = { ...sampleIncident[0], state: '완료' }
+    return <IncidentCard data={data} />;
+  })
